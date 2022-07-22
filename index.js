ed3d4ec67c401ec94853ac1206af42cca973068b
z <zehaftatekea@gmail.com>
2022-07-21 22:06:14 -0700

    remove dist

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-import fs from 'fs';
-const path = require('path');
+
 const core = require('@actions/core');
+var packageInfo = require('./package.json');
 
-/**
- * Read package.json file
- * @param {string} path
- * @returns {object}
- */
-const readPackageJson = function (path1) {
-    const packageJson = fs.readFileSync(path.join(path1, 'package.json')).toString();
-
-    return JSON.parse(packageJson);
-};
 
 try {
    
-    const packageInfo = readPackageJson(path);
-
     core.setOutput("name", packageInfo.name);
     core.setOutput("version", packageInfo.version);
 } catch (error) {
